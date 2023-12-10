4e548bc7f5bd8f1e7ee96975ec994a41f92135a3
ricargod <142942070+ricargod@users.noreply.github.com>
2023-12-10 01:45:06 +0000

    sensor inicializa com intervalos de tempo

--- data_processor.cpp
+++ data_processor.cpp
@@ -2,7 +2,9 @@
 #include <cstdlib>
 #include <chrono>
 #include <thread>
+#include <mutex>
 #include <unistd.h>
+#include <vector>
 #include "json.hpp" 
 #include "mqtt/client.h" 
 
@@ -10,6 +12,8 @@
 #define BROKER_ADDRESS "tcp://localhost:1883"
 #define GRAPHITE_HOST "graphite"
 #define GRAPHITE_PORT 2003
+
+std::mutex m;
 
 void post_metric(const std::string& machine_id, const std::string& sensor_id, const std::string& timestamp_str, const int value) {
 
@@ -25,9 +29,31 @@ std::vector<std::string> split(const std::string &str, char delim) {
     return tokens;
 }
 
+std::string clientId = "clientId";
+mqtt::async_client client(BROKER_ADDRESS, clientId);
+
+//Guarda o valor do timestamp da ultima mensagem recebida de cada sensor
+//encadeado como: {id_do_sensor: ultimo_timestamp}
+std::map<std::string, std::string> actual_timestamps;
+
+void monitor_sensor_inactivity(std::string sensorId, int data_interval) {
+    while(1) {
+        
+        std::cout << actual_timestamps[sensorId] << std::endl;
+        auto timestamp_parts = split(actual_timestamps[sensorId], 'T');
+        std::string time = timestamp_parts[1];
+        auto time_parts = split(time, ':');
+        std::cout << time_parts[2] << std::endl;
+       
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        
+    }
+    
+}
+
 int main(int argc, char* argv[]) {
-    std::string clientId = "clientId";
-    mqtt::async_client client(BROKER_ADDRESS, clientId);
+
+    
 
     // Create an MQTT callback.
     class callback : public virtual mqtt::callback {
@@ -36,16 +62,60 @@ int main(int argc, char* argv[]) {
         void message_arrived(mqtt::const_message_ptr msg) override {
             auto j = nlohmann::json::parse(msg->get_payload());
 
+            //std::cout << "topico: " << msg->get_topic() << "    payload: " << msg->get_payload() << std::endl;
+
+            
+
+            if (msg->get_topic() == "/sensor_monitors") {
+                
+                
+                std::string new_machine_id = j["machine_id"];
+                
+                std::string new_sensor1_id = j["sensors"][0]["sensor_id"];
+                std::string new_sensor1_data_type = j["sensors"][0]["data_type"];
+                int new_sensor1_interval = j["sensors"][0]["data_interval"];
+
+                std::string new_sensor2_id = j["sensors"][1]["sensor_id"];
+                std::string new_sensor2_data_type = j["sensors"][1]["data_type"];
+                int new_sensor2_interval = j["sensors"][1]["data_interval"];
+
+                std::string topic1 = "/sensor_monitors/" + new_machine_id + "/" + new_sensor1_id;
+                std::string topic2 = "/sensor_monitors/" + new_machine_id + "/" + new_sensor2_id;
+
+                client.subscribe(topic1, QOS);
+                client.subscribe(topic2, QOS);
+
+                actual_timestamps.insert_or_assign(new_sensor1_id, "0T00:00:00");
+                //actual_timestamps.insert_or_assign(new_sensor2_id, "0");
+
+                std::thread m_i_1(monitor_sensor_inactivity, new_sensor1_id , new_sensor1_interval);
+                m_i_1.detach();
+                //std::thread m_i_2(monitor_sensor_inactivity, new_sensor2_id, new_sensor2_interval);
+                //m_i_2.detach();
+            
+
+            }
+
+            else {
+            
             std::string topic = msg->get_topic();
             auto topic_parts = split(topic, '/');
             std::string machine_id = topic_parts[2];
             std::string sensor_id = topic_parts[3];
 
             std::string timestamp = j["timestamp"];
-            int value = j["value"];
+            double value = j["value"];
+    
+            
+            actual_timestamps.insert_or_assign(sensor_id, timestamp);
+           
+
             post_metric(machine_id, sensor_id, timestamp, value);
+            }
         }
     };
+
+    
 
     callback cb;
     client.set_callback(cb);
@@ -56,14 +126,16 @@ int main(int argc, char* argv[]) {
     connOpts.set_clean_session(true);
 
     try {
-        client.connect(connOpts);
-        client.subscribe("/sensors/#", QOS);
+        client.connect(connOpts)->wait();
+        client.subscribe("/sensor_monitors", QOS);
     } catch (mqtt::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 
     while (true) {
+        std::cout << actual_timestamps["sensor1"] << std::endl;
+
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
--- sensor_monitor.cpp
+++ sensor_monitor.cpp
@@ -1,20 +1,122 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <chrono>
 #include <ctime>
 #include <thread>
+#include <mutex>
 #include <unistd.h>
 #include "json.hpp" // json handling
 #include "mqtt/client.h" // paho mqtt
 #include <iomanip>
 
+
 #define QOS 1
 #define BROKER_ADDRESS "tcp://localhost:1883"
 
-int main(int argc, char* argv[]) {
-    std::string clientId = "sensor-monitor";
-    mqtt::client client(BROKER_ADDRESS, clientId);
+std::string clientId = "sensor-monitor";
+mqtt::client client(BROKER_ADDRESS, clientId);
 
+std::mutex m;
+
+double get_cpu_frequency() {
+    std::ifstream cpuinfoFile("/proc/cpuinfo");
+    std::string line;
+    double frequency = 0;
+
+    if (cpuinfoFile.is_open()) {
+        while (std::getline(cpuinfoFile, line)) {
+            if (line.find("cpu MHz") != std::string::npos) {
+                size_t pos = line.find(":");
+                if (pos != std::string::npos) {
+                    std::string freqStr = line.substr(pos + 1);
+                    frequency = std::stod(freqStr);
+                    break; // Assuming all cores have the same frequency
+                }
+            }
+        }
+        cpuinfoFile.close();
+    }
+    return(frequency);
+}
+
+double get_cpu_usage() {
+    std::ifstream statFile("/proc/stat");
+    std::string line;
+
+    double cpuUsage = 0;
+
+    if (statFile.is_open()) {
+        std::getline(statFile, line);
+        statFile.close();
+
+        if (line.substr(0, 3) == "cpu") {
+            std::istringstream iss(line);
+            std::string cpuLabel;
+            iss >> cpuLabel;
+
+            if (cpuLabel == "cpu") {
+                // Extrair informações sobre o tempo de CPU
+                unsigned long user, nice, system, idle;
+                iss >> user >> nice >> system >> idle;
+
+                unsigned long totalTime = user + nice + system + idle;
+                cpuUsage = 100.0 * (totalTime - idle) / totalTime;
+            }
+        }
+    }
+    return cpuUsage;
+}
+
+void read_and_publish_sensor(std::string machineId, std::string sensorId, int data_interval) {
+
+    while(1) {
+
+    // Get the current time in ISO 8601 format.
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+    std::tm* now_tm = std::localtime(&now_c);
+    std::stringstream ss;
+    ss << std::put_time(now_tm, "%FT%TZ");
+    std::string timestamp = ss.str();
+
+    double value = 0;
+
+    if(sensorId == "sensor1") {
+        value = get_cpu_frequency();
+    }
+    else if(sensorId == "sensor2") {
+        value = get_cpu_usage();
+    }
+    
+    
+
+    // Construct the JSON message.
+    nlohmann::json j;
+    j["timestamp"] = timestamp;
+    j["value"] = value;
+
+    // Publish the JSON message to the appropriate topic.
+    std::string topic = "/sensor_monitors/" + machineId + "/" + sensorId;
+    mqtt::message msg(topic, j.dump(), QOS, false);
+    std::clog << "message published - topic: " << topic << " - message: " << j.dump() << std::endl;
+
+    m.lock();
+    client.publish(msg);
+    m.unlock();
+   
+    std::this_thread::sleep_for(std::chrono::milliseconds(data_interval));
+    } 
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cerr << "Uso: " << argv[0] << " <data_interval1> <data_interval2>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int data_interval1 = std::atoi(argv[1]);
+    int data_interval2 = std::atoi(argv[2]);
     // Connect to the MQTT broker.
     mqtt::connect_options connOpts;
     connOpts.set_keep_alive_interval(20);
@@ -32,33 +134,34 @@ int main(int argc, char* argv[]) {
     char hostname[1024];
     gethostname(hostname, 1024);
     std::string machineId(hostname);
+    std::string sensor_id1 = "sensor1";
+    std::string sensor_id2 = "sensor2";
 
-    while (true) {
-       // Get the current time in ISO 8601 format.
-        auto now = std::chrono::system_clock::now();
-        std::time_t now_c = std::chrono::system_clock::to_time_t(now);
-        std::tm* now_tm = std::localtime(&now_c);
-        std::stringstream ss;
-        ss << std::put_time(now_tm, "%FT%TZ");
-        std::string timestamp = ss.str();
+    nlohmann::json j_sensor1;
+    j_sensor1["sensor_id"] = sensor_id1;
+    j_sensor1["data_type"] = "cpu_frequency";
+    j_sensor1["data_interval"] = data_interval1;
 
-        // Generate a random value.
-        int value = rand();
+    nlohmann::json j_sensor2;
+    j_sensor2["sensor_id"] = sensor_id2;
+    j_sensor2["data_type"] = "cpu_usage";
+    j_sensor2["data_interval"] = data_interval2;
 
-        // Construct the JSON message.
-        nlohmann::json j;
-        j["timestamp"] = timestamp;
-        j["value"] = value;
+    nlohmann::json j_inicial;
+    j_inicial["machine_id"] = machineId;
+    j_inicial["sensors"] = {j_sensor1, j_sensor2};
 
-        // Publish the JSON message to the appropriate topic.
-        std::string topic = "/sensors/" + machineId + "/rand";
-        mqtt::message msg(topic, j.dump(), QOS, false);
-        std::clog << "message published - topic: " << topic << " - message: " << j.dump() << std::endl;
-        client.publish(msg);
+    std::string topic_inicial = "/sensor_monitors";
+    mqtt::message msg_inicial(topic_inicial, j_inicial.dump(), QOS, false);
+    //std::clog << "message published - topic: " << topic_inicial << " - message: " << j_inicial.dump() << std::endl;
+    client.publish(msg_inicial);
 
-        // Sleep for some time.
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
+    
+    std::thread t_sensor1(read_and_publish_sensor, machineId, sensor_id1, data_interval1);
+    std::thread t_sensor2(read_and_publish_sensor, machineId, sensor_id2, data_interval2);
 
+    t_sensor1.join();
+    t_sensor2.join();
+    
     return EXIT_SUCCESS;
 }
